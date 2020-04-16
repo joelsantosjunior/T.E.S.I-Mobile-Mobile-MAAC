b532e28343ac924cfd1291d2ad2406d52deeeefb
Joel S <joel@upplify.com>
2020-04-16 01:37:49 -0300

    Add new images and fix some alignments

--- lib/screens/tela_buscar_beacon.dart
+++ lib/screens/tela_buscar_beacon.dart
@@ -17,44 +17,50 @@ class BuscaBeacon extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.amber[400],
-      body: Center(
-        child: Stack(
-          children: <Widget>[
-            Container(
-              child: FlareActor(
-                "lib/imagens/buscando.flr",
-                animation: "Record2",
-                fit: BoxFit.contain,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+                  image: AssetImage("lib/imagens/bg.png"),
+                  fit: BoxFit.fill)),
+        child: Center(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                child: FlareActor(
+                  "lib/imagens/buscando.flr",
+                  animation: "Record2",
+                  fit: BoxFit.contain,
+                ),
               ),
-            ),
-            Container(
-              child: Align(
-                alignment: AlignmentDirectional(0, 0.25),
-                child: Text(
-                  "  Buscando...",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    decoration: TextDecoration.none,
-                    fontSize: 30,
-                    color: Colors.black,
+              Container(
+                child: Align(
+                  alignment: AlignmentDirectional(0, 0.3),
+                  child: Text(
+                    "Aproxime-se de um item em exposição",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      decoration: TextDecoration.none,
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Container(
-              child: Align(
-                alignment: AlignmentDirectional(0, 0),
-                child: FlatButton(
-                  child: Icon(
-                    Icons.search,
-                    color: Colors.black,
-                    size: 100.0,
+              Container(
+                child: Align(
+                  alignment: AlignmentDirectional(0, 0),
+                  child: FlatButton(
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.black,
+                      size: 100.0,
+                    ),
+                    onPressed: (){},
                   ),
-                  onPressed: (){},
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
