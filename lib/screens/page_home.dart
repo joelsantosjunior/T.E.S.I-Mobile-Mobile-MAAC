d42a3244ba353835abb07b0077b224a4e1e05297
Felipe Malheiros <felipemalheirosb@gmail.com>
2020-04-15 22:41:20 -0300

    Beacon Service para busca de beacons

--- lib/screens/page_home.dart
+++ lib/screens/page_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:maac_app/components/my_page_home.dart';
+import 'package:maac_app/screens/tela_buscar_beacon.dart';
 
 class PageHome extends StatelessWidget {
   @override
@@ -7,7 +8,7 @@ class PageHome extends StatelessWidget {
     return MaterialApp(
       title: 'MAAC',
       debugShowCheckedModeBanner: false,
-      home: MyPageHome(),
+      home: BuscaBeacon()
     );
   }
 }
