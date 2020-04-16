98733405d1a48e1b8ddb9df63e7efa841c820114
Yago Muniz <yagomunizns@gmail.com>
2020-04-16 02:17:44 -0300

    Vinculado tela de busca com informações do beacon

--- lib/screens/page_info_beacon.dart
+++ lib/screens/page_info_beacon.dart
@@ -1,11 +1,18 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:maac_app/models/Beacon.dart';
 
 class PageInfoBeacon extends StatelessWidget {
+  final Beacon beacon;
+
+  PageInfoBeacon({@required this.beacon});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.amber[400],
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
           SizedBox(
             height: 100,
@@ -14,34 +21,47 @@ class PageInfoBeacon extends StatelessWidget {
             child: Padding(
               padding: EdgeInsets.all(5),
               child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Image.asset(
-                  'lib/imagens/noite_estrelada.jpg',
-                  fit: BoxFit.fill
-                )
-              ),
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.asset('lib/imagens/noite_estrelada.jpg',
+                      fit: BoxFit.fill)),
             ),
           ),
           Container(
-            color: Colors.black.withOpacity(.6),
             child: Padding(
               padding: EdgeInsets.all(25),
-              child: Text(
-                "Noite Estrelada é uma pintura de Vicent van Gogh de 1889. A obra retrata a vista da janela de um quarto do hospício de Saint-Rémy-de-Provence.",
-                style: TextStyle(color: Colors.white, fontSize: 25),
+              child: SingleChildScrollView(
+                child: AutoSizeText(
+                  beacon.conteudo,
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  maxLines: 7,
+                )
               ),
-            )
+            ),
+            color: Colors.black.withOpacity(.6),
           ),
           Container(
             child: Align(
-              alignment: AlignmentDirectional(0, 0),
               child: BottomNavigationBar(
                 type: BottomNavigationBarType.fixed,
                 backgroundColor: Colors.amber[400],
                 items: [
-                  BottomNavigationBarItem(icon: Icon(Icons.reply, color: Color.fromARGB(255, 0, 0, 0), size: 40.0), title: Text('')),
-                  BottomNavigationBarItem(icon: CircleAvatar(radius: 30, backgroundColor: Colors.black, child: IconButton(iconSize: 40, icon: Icon(Icons.search,color: Colors.white)),), title: Text('')),
-                  BottomNavigationBarItem(icon: Icon(Icons.highlight_off, color: Color.fromARGB(255, 0, 0, 0), size: 40.0), title: Text(''))
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.reply,
+                          color: Color.fromARGB(255, 0, 0, 0), size: 40.0),
+                      title: Text('')),
+                  BottomNavigationBarItem(
+                      icon: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.black,
+                        child: IconButton(
+                            iconSize: 40,
+                            icon: Icon(Icons.search, color: Colors.white)),
+                      ),
+                      title: Text('')),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.highlight_off,
+                          color: Color.fromARGB(255, 0, 0, 0), size: 40.0),
+                      title: Text(''))
                 ],
               ),
             ),
