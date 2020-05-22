61b2c43aab8dad2e768e7b0106c3a97aae324c7a
Joel S. Junior <joelsantosjunior1996@gmail.com>
2020-05-21 22:33:44 -0300

    update the beacon token

--- lib/api/BeaconService.dart
+++ lib/api/BeaconService.dart
@@ -6,29 +6,23 @@ import 'package:http/http.dart' as http;
 import 'package:maac_app/models/Beacon.dart';
 
 class BeaconService {
-  
-  String token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlOTM3ZGFhZDZlMDdmMDAxNzQ0MWY3MyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTU4NjcyODM0NSwiZXhwIjoxNTg5MzIwMzQ1fQ.QJohZgckYm1V9-sW6ZEKy3zOKFRJbbZt_ocTKCBTLyc";
+  String token =
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlOTM3ZGFhZDZlMDdmMDAxNzQ0MWY3MyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTU5MDExMDQxMiwiZXhwIjoxNTkyNzAyNDEyfQ.3_m2gqTtUIGGNwftHYrTdr1KFDMcuvIsRJ__AFWnxjY";
 
-  
-   Future<List<Beacon>> getAllBeacon() async{
+  Future<List<Beacon>> getAllBeacon() async {
     final response = await http.get(
       'https://thawing-crag-60834.herokuapp.com/beacons',
-      headers: {
-        HttpHeaders.authorizationHeader:token
-      },
+      headers: {HttpHeaders.authorizationHeader: token},
     );
     Iterable list = json.decode(response.body);
     return list.map((model) => Beacon.fromJson(model)).toList();
   }
 
-
-   Future<Beacon> getBeaconById(String id) async{
+  Future<Beacon> getBeaconById(String id) async {
     final response = await http.get(
       'https://thawing-crag-60834.herokuapp.com/beacons' + '/' + id,
-      headers: {
-        HttpHeaders.authorizationHeader:token
-      },
+      headers: {HttpHeaders.authorizationHeader: token},
     );
-    return Beacon.fromJson(json.decode(response.body)); 
+    return Beacon.fromJson(json.decode(response.body));
   }
 }
